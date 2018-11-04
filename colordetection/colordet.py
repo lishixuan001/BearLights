71d159cf85887dd4c036fa8b99e16741111309da
Andrew Lou <andy.lou98@gmail.com>
2018-11-04 01:25:07 -0700

    added grading functionality on two images

--- colordetection/colordet.py
+++ colordetection/colordet.py
@@ -7,43 +7,43 @@ import numpy as np
 
 def detect_properties(path):
     """Detects image properties in the file."""
+    from google.cloud import vision
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
 
     image = vision.types.Image(content=content)
-    print(image)
 
     response = client.image_properties(image=image)
     props = response.image_properties_annotation
-    print('Properties:')
+    # print('Properties:')
 
-    for color in props.dominant_colors.colors:
-        print('fraction: {}'.format(color.pixel_fraction))
-        print('\tr: {}'.format(color.color.red))
-        print('\tg: {}'.format(color.color.green))
-        print('\tb: {}'.format(color.color.blue))
-        print('\ta: {}'.format(color.color.alpha))
+    # for color in props.dominant_colors.colors:
+    #     print('fraction: {}'.format(color.pixel_fraction))
+    #     print('\tr: {}'.format(color.color.red))
+    #     print('\tg: {}'.format(color.color.green))
+    #     print('\tb: {}'.format(color.color.blue))
+    #     print('\ta: {}'.format(color.color.alpha))
 
+    return [[color.pixel_fraction, color.color.red, color.color.green, color.color.blue] for color in props.dominant_colors.colors]
 
-def main():
-    # detect_properties("../facedetector/test.jpeg")
-    client = vision.ImageAnnotatorClient()
-    img = cv2.imread(os.path.join(sys.path[0], "../facedetector/test.jpeg"))
-    image = vision.types.Image(content=img.tobytes())
-    print(image)
-    response = client.image_properties(image=image)
-    props = response.image_properties_annotation
-    print('Properties:')
-
-    for color in props.dominant_colors.colors:
-        print('fraction: {}'.format(color.pixel_fraction))
-        print('\tr: {}'.format(color.color.red))
-        print('\tg: {}'.format(color.color.green))
-        print('\tb: {}'.format(color.color.blue))
-        print('\ta: {}'.format(color.color.alpha))
-
+def get_grade(path1, path2):
+    """
+        Grades the image at path2 based on the differences in properties of the image at path1
+    """
+    color_properties_1 = np.array(max(detect_properties(path1), key=lambda x: x[0]))
+    color_properties_2 = np.array(max(detect_properties(path2), key=lambda x: x[0]))
+    # while len(color_properties_2) != len(color_properties_1):
+    #     if len(color_properties_1) > len(color_properties_2):
+    #         color_properties_2 += [0, 0, 0, 0]
+    #     elif len(color_properties_2) > len(color_properties_1):
+    #         color_properties_1 += [0, 0, 0, 0]
+    return 1 - np.linalg.norm(color_properties_1 - color_properties_2)/441.672956
 
 if __name__ == "__main__":
-    main()
+    white = "/users/andrewlou/Desktop/white-square.png"
+    black = "/users/andrewlou/Desktop/black-square.jpg"
+    print("White square with black square:" + str(get_grade(white, black)))
+    print("White square with white square:" + str(get_grade(white, white)))
+    print("Black square with black square:" + str(get_grade(black, black)))
